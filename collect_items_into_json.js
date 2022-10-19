de21642c13f7085a6994486c34be0045e5f2ec1d
3virgula14 <3virgula14de96@gmail.com>
2022-10-19 19:23:23 -0300

    rt

--- collect_items_into_json.js
+++ collect_items_into_json.js
@@ -9,7 +9,7 @@ fs.readdir(__dirname+"/public/portfolio_items", (err, files) => {
             const data = fs.readFileSync(__dirname+"/public/portfolio_items/"+file, 'utf8');
             fileContentArr+= data+", ";
         })
-        let finaljson = JSON.parse(`{"portfolio":[${fileContentArr}]}`)
+        let finaljson = JSON.parse(`{"portfolio":[${fileContentArr.slice(0, -1)}]}`)
         fs.writeFile(__dirname+"/public/data.json", JSON.stringify(finaljson), (err) =>{})
     }
   })
