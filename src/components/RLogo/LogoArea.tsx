2cfb83620b9e2707996fe468afe593b2e5d86711
3virgula14 <3virgula14de96@gmail.com>
2022-10-19 21:22:15 -0300

    231

--- src/components/RLogo/LogoArea.tsx
+++ src/components/RLogo/LogoArea.tsx
@@ -1,15 +1,17 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, JSXNode } from "@builder.io/qwik";
 
-export default component$(()=>{
-    return (<div id="img" class="activated">
+export default component$(():JSXNode<any>=>{
+    
+    return (
+    <div id="img" class="activated">
+        <img onClick$={(e)=> {
+            const parentNode = ((e.target as HTMLElement).parentNode as any)
+            const contains = parentNode.classList.contains("activated");
+            parentNode.classList[`${contains? "remove": "add"}`]("activated");
+            
+        }} src={"/favicon.png"} />
         <ul>
             <li>Site em construção</li>
         </ul>
-    <img onClick$={(e)=> {
-        const parentNode = ((e.target as HTMLElement).parentNode as any)
-        const contains = parentNode.classList.contains("activated");
-        parentNode.classList[`${contains? "remove": "add"}`]("activated");
-        
-    }} src={"/favicon.png"} />
-</div>)
+    </div>)
 })
